876bc79ca54536cef4c902df5fbccec493e520ea
Ivan Moiseev <ivanmois2002@gmail.com>
2025-03-22 00:20:40 +0100

    feat: add better tests for postgres types

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -133,3 +133,49 @@ fn out_table_is_not_empty(
 
     assert!(db_mover::run(args.clone()).is_err());
 }
+
+#[rstest]
+#[case("bigint", "9223372036854775800", "9223372036854775800")]
+#[case("double precision", "123.12345678", "123.12345678")]
+#[case("double precision", "123.12345678", "123.12345678")]
+#[case("varchar(10)", "'test'", "test")]
+#[case("char(10)", "'test'", "test")]
+#[case("bpchar", "'test'", "test")]
+#[case("text", "'test'", "test")]
+#[case("bytea", "'test'", "\\x74657374")] // Hex output
+#[case("timestamp", "'2004-10-19 10:23:54'", "2004-10-19 10:23:54")]
+fn postgres_types_compatability(
+    #[case] type_name: &str,
+    #[case] value: &str,
+    #[case] expected: &str,
+) {
+    let mut in_db = TestPostresDatabase::new();
+    let mut out_db = TestPostresDatabase::new();
+    let create_table_query = format!("CREATE TABLE test (field {type_name})");
+    in_db.client.execute(&create_table_query, &[]).unwrap();
+    out_db.client.execute(&create_table_query, &[]).unwrap();
+
+    in_db
+        .client
+        .execute("INSERT INTO test VALUES (NULL)", &[])
+        .unwrap();
+    let insert_query = format!("INSERT INTO test VALUES ({value})");
+    in_db.client.execute(&insert_query, &[]).unwrap();
+
+    let mut args = db_mover::args::Args::new(in_db.get_uri(), out_db.get_uri());
+    args.table.push("test".to_string());
+
+    db_mover::run(args.clone()).unwrap();
+
+    let mut expected = vec![None, Some(expected.to_string())];
+    expected.sort();
+    let mut result: Vec<Option<String>> = out_db
+        .client
+        .query("SELECT CAST(field as TEXT) FROM test", &[])
+        .unwrap()
+        .iter()
+        .map(|row| row.get::<_, Option<String>>(0))
+        .collect();
+    result.sort();
+    assert_eq!(expected, result);
+}
